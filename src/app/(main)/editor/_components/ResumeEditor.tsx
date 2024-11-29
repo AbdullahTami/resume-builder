05c16ecf059fc249d0fd38f5af24d6b9cc6c7dbf
abdullahqayid <3boo0odq0v@gmail.com>
2024-11-29 18:03:38 +0300

    general info form done

--- src/app/(main)/editor/_components/ResumeEditor.tsx
+++ src/app/(main)/editor/_components/ResumeEditor.tsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import GeneralInfoForm from "./forms/GeneralInfoForm";
 
 export default function ResumeEditor() {
   return (
@@ -15,7 +16,10 @@ export default function ResumeEditor() {
       </header>
       <main className="relative grow">
         <div className="absolute bottom-0 top-0 flex w-full overflow-auto">
-          <div className="w-full md:w-1/2">Left</div>
+          <div className="w-full p-3 md:w-1/2">
+            <GeneralInfoForm />
+          </div>
+          <div className="grow md:border-r" />
           <div className="hidden w-1/2 md:flex">Right</div>
         </div>
       </main>
