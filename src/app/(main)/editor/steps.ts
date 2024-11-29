086998dcb2c91b29dc96ef83510881e27b05ddf5
abdullahqayid <3boo0odq0v@gmail.com>
2024-11-29 22:34:49 +0300

    storing data in state to show resume preview done

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -1,9 +1,10 @@
+import { EditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./_components/forms/GeneralInfoForm";
 import PersonalInfoForm from "./_components/forms/PersonalInfoForm";
 
 export const steps: {
   title: string;
-  component: React.ComponentType;
+  component: React.ComponentType<EditorFormProps>;
   key: string;
 }[] = [
   {
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -29,3 +29,13 @@ export const personalInfoSchema = z.object({
   email: optionalString,
 });
 export type PersonalInfoValues = z.infer<typeof personalInfoSchema>;
+
+export const resumeSchema = z.object({
+  ...generalInfoSchema.shape,
+  ...personalInfoSchema.shape,
+});
+
+export type ResumeValues = Omit<z.infer<typeof resumeSchema>, "photo"> & {
+  id?: string;
+  photo?: File | string | null;
+};
