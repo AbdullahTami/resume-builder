22a4a82d2d4739bd8e7f6c9dc620c013106fa86e
abdullahqayid <3boo0odq0v@gmail.com>
2024-11-29 21:37:30 +0300

    footer functionality done

--- src/app/(main)/editor/_components/ResumeEditor.tsx
+++ src/app/(main)/editor/_components/ResumeEditor.tsx
@@ -1,12 +1,8 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import React from "react";
-import GeneralInfoForm from "./forms/GeneralInfoForm";
-import PersonalInfoForm from "./forms/PersonalInfoForm";
 import { useSearchParams } from "next/navigation";
 import { steps } from "../steps";
 import Breadcrumbs from "./Breadcrumbs";
+import Footer from "./Footer";
 
 export default function ResumeEditor() {
   const searchParams = useSearchParams();
@@ -42,20 +38,7 @@ export default function ResumeEditor() {
           <div className="hidden w-1/2 md:flex">Right</div>
         </div>
       </main>
-      <footer className="w-full border-t px-3 py-5">
-        <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
-          <div className="flex items-center gap-3">
-            <Button variant="secondary">Previous step</Button>
-            <Button variant="default">Next step</Button>
-          </div>
-          <div className="flex items-center gap-3">
-            <Button variant="secondary" asChild>
-              <Link href="/resumes">Close</Link>
-            </Button>
-            <p className="text-muted-foreground opacity-0">Saving...</p>
-          </div>
-        </div>
-      </footer>
+      <Footer currentStep={currentStep} setCurrentStep={setStep} />
     </div>
   );
 }
