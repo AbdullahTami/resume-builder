5a11fe4a4cb07b1c9e04a19533f4e989d7c10775
abdullahqayid <3boo0odq0v@gmail.com>
2024-12-31 10:20:53 +0300

    deleting info image initial logic done

--- src/app/(main)/editor/_components/forms/PersonalInfoForm.tsx
+++ src/app/(main)/editor/_components/forms/PersonalInfoForm.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,7 +11,7 @@ import { Input } from "@/components/ui/input";
 import { EditorFormProps } from "@/lib/types";
 import { personalInfoSchema, PersonalInfoValues } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 
 export default function PersonalInfoForm({
@@ -38,6 +39,8 @@ export default function PersonalInfoForm({
     });
     return unsubscribe;
   }, [form, resumeData, setResumeData]);
+
+  const photoInfoRef = useRef<HTMLInputElement>(null);
   return (
     <div className="m-auto max-w-xl space-y-6">
       <div className="space-y-1.5 text-center">
@@ -52,17 +55,32 @@ export default function PersonalInfoForm({
             render={({ field: { value, ...fieldValues } }) => (
               <FormItem>
                 <FormLabel>Your photo</FormLabel>
-                <FormControl>
-                  <Input
-                    {...fieldValues}
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => {
-                      const file = e.target.files?.[0];
-                      fieldValues.onChange(file);
+                <div className="flex items-center gap-2">
+                  <FormControl>
+                    <Input
+                      {...fieldValues}
+                      type="file"
+                      accept="image/*"
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        fieldValues.onChange(file);
+                      }}
+                      ref={photoInfoRef}
+                    />
+                  </FormControl>
+                  <Button
+                    variant="secondary"
+                    type="button"
+                    onClick={() => {
+                      fieldValues.onChange(null);
+                      if (photoInfoRef.current) {
+                        photoInfoRef.current.value = "";
+                      }
                     }}
-                  />
-                </FormControl>
+                  >
+                    Remove
+                  </Button>
+                </div>
                 <FormMessage />
               </FormItem>
             )}
